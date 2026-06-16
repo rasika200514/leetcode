c591ffbff88362602e7c6e020be5fff2a70cfed3
rasika200514 <rasika200514@gmail.com>
2026-06-16 11:08:29 +0530

    Time: 0 ms (100%), Space: 42.7 MB (83.61%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String s) {
+     if(s.length()<26){
+        return false;
+     }   
+     for(char i='a';i<='z';i++){
+        if(s.indexOf(i)<0){
+            return false;
+        }
+     }
+     return true;
+    }
+}
